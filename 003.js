5f1d6eb67f9f0ad91ca2791f85b8a917c3181491
tood21 <tood2111@gmail.com>
2021-12-01 21:39:06 +0900

    003

---
+++ 003.js
@@ -0,0 +1,15 @@
+// 다음 출력 값으로 올바른 것은?
+
+// jsx
+// var arr = [100, 200, 300];
+// console.log(typeof(arr));
+
+// 1)  undefined
+// 2)  string
+// 3)  number
+// 4)  object
+
+var arr = [100, 200, 300];
+console.log(typeof arr);
+
+// 답은 : object
