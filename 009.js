f21df46fd3bb2abf51a6e019d5bc522dee9c5841
tood21 <tood2111@gmail.com>
2021-12-03 20:37:14 +0900

    009

---
+++ 009.js
@@ -0,0 +1,16 @@
+데이터
+var year = '2019';
+var month = '04';
+var day = '26';
+var hour = '11';
+var minute = '34';
+var second = '27';
+
+var result = year.concat("/"+month).concat("/"+day).concat(" " +hour).concat(":"+minute).concat(":"+second); // 내가 쓴 답 ;;
+
+
+//var result = year.concat('/', month, '/', day, ' ', hour, ':', minute, ':', second);   답안지의 답
+console.log(result);
+
+출력
+2019/04/26 11:34:27
